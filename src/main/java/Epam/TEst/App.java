d7fc77bf6eb80c38c87eecfe0fb3e766a46a187c
Phushyamithra <60391390+Phushyamithra@users.noreply.github.com>
2020-07-19 07:06:52 +0530

    Add files via upload
    Upload using GIT commands

---
+++ src/main/java/Epam/TEst/App.java
@@ -0,0 +1,13 @@
+package Epam.TEst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
